51c6d56d9dec220a5e0752feccb1fa799d9f7dde
chak1988 <88chak88@gmail.com>
2021-12-13 16:05:55 +0200

    important commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def get_random(n):
+def get_random(n, s):
     import random
-    return random.randint(0,n)
+    return random.randint(s,n)
+
 
 if __name__ == '__main__':
     print('Hello master git')
+    get_random(20, 30)
